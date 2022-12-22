e33160f31df46853c928760f501b05785d890f7b
Ishan <88707114+Ishan1712@users.noreply.github.com>
2022-12-22 20:37:38 +0530

    Add files via upload

---
+++ PROJECT2_UNIT CONVERTOR/script.js
@@ -0,0 +1,110 @@
+var property = new Array();
+var unit = new Array();
+var factor = new Array();
+
+property[0] = "Area";
+unit[0] = new Array("Square meter (m^2)", "Acre (acre)", "Are", "Barn (barn)", "Hectare", "Rood", "Square centimeter", "Square kilometer", "Circular mil", "Square foot (ft^2)", "Square inch (in^2)", "Square mile (mi^2)", "Square yard (yd^2)");
+factor[0] = new Array(1, 4046.856, 100, 1E-28, 10000, 1011.71413184285, .0001, 1000000, 5.067075E-10, 9.290304E-02, 6.4516E-04, 2589988, .8361274);
+
+property[1] = "Electricity";
+unit[1] = new Array("Coulomb (Cb)", "Abcoulomb", "Ampere hour (A hr)", "Faraday (F)", "Statcoulomb", "Millifaraday (mF)", "Microfaraday (mu-F)", "Picofaraday (pF)");
+factor[1] = new Array(1, 10, 3600, 96521.8999999997, .000000000333564, 96.5219, 9.65219E-02, 9.65219E-05);
+
+property[2] = "Energy";
+unit[2] = new Array("Joule (J)", "BTU (mean)", "BTU (thermochemical)", "Calorie (SI) (cal)", "Calorie (mean)(cal)", "Calorie (thermo)", "Electron volt (eV)", "Erg (erg)", "Foot-pound force", "Foot-poundal", "Horsepower-hour", "Kilocalorie (SI)(kcal)", "Kilocalorie (mean)(kcal)", "Kilowatt-hour (kW hr)", "Ton of TNT", "Volt-coulomb (V Cb)", "Watt-hour (W hr)", "Watt-second (W sec)");
+factor[2] = new Array(1, 1055.87, 1054.35, 4.1868, 4.19002, 4.184, 1.6021E-19, .0000001, 1.355818, 4.214011E-02, 2684077.3, 4186.8, 4190.02, 3600000, 4.2E9, 1, 3600, 1);
+
+property[3] = "Temperature";
+unit[3] = new Array("Degrees Celsius ('C)", "Degrees Fahrenheit ('F)", "Degrees Kelvin ('K)", "Degrees Rankine ('R)");
+factor[3] = new Array(1, 0.555555555555, 1, 0.555555555555);
+tempIncrement = new Array(0, -32, -273.15, -491.67);
+
+ property[4] = "Mass";
+ unit[4] = new Array("Kilogram (kgr)", "Gram (gr)", "Milligram (mgr)", "Microgram (mu-gr)", "Carat (metric)(ct)", "Hundredweight (long)", "Hundredweight (short)", "Pound mass (lbm)", "Pound mass (troy)", "Ounce mass (ozm)", "Ounce mass (troy)", "Slug", "Ton (assay)", "Ton (long)", "Ton (short)", "Ton (metric)", "Tonne");
+ factor[4] = new Array(1, .001, 1e-6, .000000001, .0002, 50.80235, 45.35924, .4535924, .3732417, .02834952, .03110348, 14.5939, .02916667, 1016.047, 907.1847, 1000, 1000);
+
+ property[5] = "Density & Mass capacity";
+ unit[5] = new Array("Kilogram/cub.meter", "Grain/galon", "Grams/cm^3 (gr/cc)", "Pound mass/cubic foot", "Pound mass/cubic-inch", "Ounces/gallon (UK,liq)", "Ounces/gallon (US,liq)", "Ounces (mass)/inch", "Pound mass/gal (UK,liq)", "Pound mass/gal (US,liq)", "Slug/cubic foot", "Tons (long,mass)/cub.yard");
+ factor[5] = new Array(1, .01711806, 1000, 16.01846, 27679.91, 6.236027, 7.489152, 1729.994, 99.77644, 119.8264, 515.379, 1328.939);
+ 
+ property[6] = "Power";
+ unit[6] = new Array("Watt (W)", "Kilowatt (kW)", "Megawatt (MW)", "Milliwatt (mW)", "BTU (SI)/hour", "BTU (thermo)/second", "BTU (thermo)/minute", "BTU (thermo)/hour", "Calorie (thermo)/second", "Calorie (thermo)/minute", "Erg/second", "Foot-pound force/hour", "Foot-pound force/minute", "Foot-pound force/second", "Horsepower(550 ft lbf/s)", "Horsepower (electric)", "Horsepower (boiler)", "Horsepower (metric)", "Horsepower (UK)", "Kilocalorie (thermo)/min", "Kilocalorie (thermo)/sec");
+ factor[6] = new Array(1, 1000, 1000000, .001, .2930667, 1054.35, 17.5725, .2928751, 4.184, 6.973333E-02, .0000001, .0003766161, .02259697, 1.355818, 745.7, 746, 9809.5, 735.499, 745.7, 69.7333, 4184);
+ 
+ property[7] = "Pressure & Stress";
+ unit[7] = new Array("Newton/sq.meter", "Atmosphere (normal)", "Atmosphere (techinical)", "Bar", "Centimeter mercury(cmHg)", "Centimeter water (4'C)", "Decibar", "Kgr force/sq.centimeter", "Kgr force/sq.meter", "Kip/square inch", "Millibar", "Millimeter mercury(mmHg)", "Pascal (Pa)", "Kilopascal (kPa)", "Megapascal (Mpa)", "Poundal/sq.foot", "Pound-force/sq.foot", "Pound-force/sq.inch (psi)", "Torr (mmHg,0'C)");
+ factor[7] = new Array(1, 101325, 98066.5, 100000, 1333.22, 98.0638, 10000, 98066.5, 9.80665, 6894757, 100, 133.3224, 1, 1000, 1000000, 47.88026, 47.88026, 6894.757, 133.322);
+ 
+function UpdateUnitMenu(propMenu, unitMenu) {
+    var i;
+    i = propMenu.selectedIndex;
+    FillMenuWithArray(unitMenu, unit[i]);
+  }
+  
+  function FillMenuWithArray(myMenu, myArray) {
+    var i;
+    myMenu.length = myArray.length;
+    for (i = 0; i < myArray.length; i++) {
+      myMenu.options[i].text = myArray[i];
+    }
+  }
+  
+  function CalculateUnit(sourceForm, targetForm) {
+    var sourceValue = sourceForm.unit_input.value;
+  
+    sourceValue = parseFloat(sourceValue);
+    if (!isNaN(sourceValue) || sourceValue == 0) {
+      sourceForm.unit_input.value = sourceValue;
+      ConvertFromTo(sourceForm, targetForm);
+    }
+  }
+  
+  function ConvertFromTo(sourceForm, targetForm) {
+    var propIndex;
+    var sourceIndex;
+    var sourceFactor;
+    var targetIndex;
+    var targetFactor;
+    var result;
+  
+    propIndex = document.property_form.the_menu.selectedIndex;
+    sourceIndex = sourceForm.unit_menu.selectedIndex;
+    sourceFactor = factor[propIndex][sourceIndex];
+  
+    targetIndex = targetForm.unit_menu.selectedIndex;
+    targetFactor = factor[propIndex][targetIndex];
+  
+  
+    result = sourceForm.unit_input.value;
+    if (property[propIndex] == "Temperature") {
+      result = parseFloat(result) + tempIncrement[sourceIndex];
+    }
+    result = result * sourceFactor;
+  
+    result = result / targetFactor;
+    if (property[propIndex] == "Temperature") {
+      result = parseFloat(result) - tempIncrement[targetIndex];
+    }
+  
+    targetForm.unit_input.value = result;
+  }
+  
+  window.onload = function(e) {
+    FillMenuWithArray(document.property_form.the_menu, property);
+    UpdateUnitMenu(document.property_form.the_menu, document.form_A.unit_menu);
+    UpdateUnitMenu(document.property_form.the_menu, document.form_B.unit_menu)
+  }
+  
+  document.getElementByClass('numbersonly').addEventListener('keydown', function(e) {
+    var key = e.keyCode ? e.keyCode : e.which;
+  
+    if (!([8, 9, 13, 27, 46, 110, 190].indexOf(key) !== -1 ||
+        (key == 65 && (e.ctrlKey || e.metaKey)) || 
+        (key == 67 && (e.ctrlKey || e.metaKey)) ||
+        (key == 86 && (e.ctrlKey || e.metaKey)) || 
+        (key >= 35 && key <= 40) || 
+        (key >= 48 && key <= 57 && !(e.shiftKey || e.altKey)) ||
+        (key >= 96 && key <= 105) 
+        (key == 190) 
+      )) e.preventDefault();
+  });
